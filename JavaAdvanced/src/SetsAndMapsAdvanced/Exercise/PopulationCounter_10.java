6a056dffb7076dec92ce087617bf4c4e421c3fe0
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-05-30 10:59:57 +0300

    Improved solution for PopulationCounter in SetsAndMapsAdvanced.

--- JavaAdvanced/src/SetsAndMapsAdvanced/Exercise/PopulationCounter_10.java
+++ JavaAdvanced/src/SetsAndMapsAdvanced/Exercise/PopulationCounter_10.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class PopulationCounter_10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<String, Integer> countriesPopulation = new LinkedHashMap<>();
         Map<String, Map<String, Integer>> countriesCities = new LinkedHashMap<>();
 
         String input = sc.nextLine();
@@ -18,29 +17,22 @@ public class PopulationCounter_10 {
             int population = Integer.parseInt(input.split("\\|")[2]);
             if (!countriesCities.containsKey(country)) {
                 countriesCities.put(country, new LinkedHashMap<>());
-                countriesCities.get(country).put(city, population);
-                countriesPopulation.put(country, population);
-            } else {
-                countriesCities.get(country).put(city, population);
-                countriesPopulation.put(country, countriesPopulation.get(country) + population);
             }
+            countriesCities.get(country).put(city, population);
             input = sc.nextLine();
         }
-        countriesPopulation.entrySet().stream()
-                .sorted((left, right) -> {
-                    int result = right.getValue().compareTo(left.getValue());
-                    return result;
-                }).forEach(e -> {
-            System.out.printf("%s (total population: %d)%n", e.getKey(), e.getValue());
-            countriesCities.get(e.getKey()).entrySet().stream()
-                    .sorted((left, right) -> {
-                        int result = right.getValue().compareTo(left.getValue());
-                        return result;
-                    }).forEach(d -> {
-                System.out.printf("=>%s: %d%n", d.getKey(), d.getValue());
-
-            });
-        });
+        countriesCities.entrySet().stream().sorted((left, right) ->
+                Long.compare(right.getValue().values().stream().mapToLong(e -> e).sum(),
+                        left.getValue().values().stream().mapToLong(e -> e).sum())).
+                forEach(e -> {
+                    System.out.printf("%s (total population: %d)%n", e.getKey(), e.getValue().values().stream().mapToLong(k -> k).sum());
+                    countriesCities.get(e.getKey()).entrySet().stream()
+                            .sorted((left, right) -> {
+                                return right.getValue().compareTo(left.getValue());
+                            }).forEach(d -> {
+                        System.out.printf("=>%s: %d%n", d.getKey(), d.getValue());
+                    });
+                });
     }
 }
 
