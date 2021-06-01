b0a8a484f1d078e2daee82a7613774c6cc0c0b1e
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-06-01 22:46:34 +0300

    Added problem for SortingEvenNumbers in FunctionalProgramming.

---
+++ JavaAdvanced/src/FunctionalProgramming/Lab/SortEvenNumbers_01.java
@@ -0,0 +1,27 @@
+package FunctionalProgramming.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SortEvenNumbers_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(sc.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .filter(e -> e % 2 == 0)
+                .toArray();
+        StringBuilder out =  new StringBuilder();
+        Function<IntStream, String> formatNumbers = str -> str
+                                                            .mapToObj(String::valueOf)
+                                                            .collect(Collectors.joining(", "));
+
+        String functionalOutput = formatNumbers.apply(Arrays.stream(numbers));
+        System.out.println(functionalOutput);
+        String sortedNumbers = formatNumbers.apply(Arrays.stream(numbers).sorted());
+        System.out.println(sortedNumbers);
+    }
+}
