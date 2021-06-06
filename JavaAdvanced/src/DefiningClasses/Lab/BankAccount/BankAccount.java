416e7f7b1c3d0fd0a8632fbe484603e8e57226d6
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-06-06 17:08:22 +0300

    Added problem for BankAccount in DefiningClasses exercise.

---
+++ JavaAdvanced/src/DefiningClasses/Lab/BankAccount/BankAccount.java
@@ -0,0 +1,30 @@
+package DefiningClasses.Lab.BankAccount;
+
+public class BankAccount {
+    private static int accountsCount = 1;
+    private static double interestRate = 0.02;
+
+
+    private int id;
+    private double balance;
+
+
+    public BankAccount(){
+        this.id = accountsCount;
+        accountsCount++;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+    public  double getInterest(int years){
+        return this.balance * interestRate * years;
+    }
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
