f403030445463544f5561ae367b3a7aedd74f38d
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-06-09 21:51:29 +0300

    Added problem for PokemonTrainer in DefiningClasses exercise.

---
+++ JavaAdvanced/src/DefiningClasses/Exercise/SpeedRacing_03/Car.java
@@ -0,0 +1,30 @@
+package DefiningClasses.Exercise.SpeedRacing_03;
+
+public class Car {
+    private String model;
+    private double fuelAmount;
+    private double fuelCostFor1Km;
+    private int distanceTraveled;
+
+
+    public Car(String model, double fuelAmount, double fuelCostFor1Km) {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.fuelCostFor1Km = fuelCostFor1Km;
+        this.distanceTraveled = 0;
+    }
+
+    public boolean checkForTraveling(int distance){
+        return distance * fuelCostFor1Km * 1.0 <= fuelAmount;
+    }
+
+    public void moveTheCar(int distance){
+        this.fuelAmount -= distance * this.fuelCostFor1Km;
+        this.distanceTraveled += distance;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %d", this.model, this.fuelAmount, this.distanceTraveled);
+    }
+}
---
+++ JavaAdvanced/src/DefiningClasses/Exercise/SpeedRacing_03/Main.java
@@ -0,0 +1,34 @@
+package DefiningClasses.Exercise.SpeedRacing_03;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        Map<String, Car> cars = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = sc.nextLine().split("\\s+");
+            String model = input[0];
+            double fuelAmount = Double.parseDouble(input[1]);
+            double fuelCostFor1Km = Double.parseDouble(input[2]);
+            Car car = new Car(model, fuelAmount, fuelCostFor1Km);
+            cars.put(model, car);
+        }
+        String command = sc.nextLine();
+
+        while (!command.equals("End")){
+            String model = command.split("\\s+")[1];
+            int distance = Integer.parseInt(command.split("\\s+")[2]);
+            if(!cars.get(model).checkForTraveling(distance)){
+                System.out.println("Insufficient fuel for the drive");
+            } else {
+                cars.get(model).moveTheCar(distance);
+            }
+            command = sc.nextLine();
+        }
+        cars.values().forEach(System.out::println);
+    }
+}
