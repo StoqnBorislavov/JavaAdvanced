228c741d325325c7f380135fd61f07a7e8386dca
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-06-11 22:05:24 +0300

    Added problem for JarOfT in Generics.

---
+++ JavaAdvanced/src/Generics/Lab/Jar_01/Jar.java
@@ -0,0 +1,20 @@
+package Generics.Lab.Jar_01;
+
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+
+    private ArrayDeque<T> stack;
+
+    public Jar() {
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void add(T element){
+        this.stack.push(element);
+    }
+
+    public T remove(){
+        return this.stack.pop();
+    }
+}
---
+++ JavaAdvanced/src/Generics/Lab/Jar_01/Main.java
@@ -0,0 +1,13 @@
+package Generics.Lab.Jar_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<Integer> integerJar = new Jar<>();
+
+        integerJar.add(13);
+        integerJar.add(43);
+        System.out.println(integerJar.remove());
+        System.out.println(integerJar.remove());
+    }
+}
