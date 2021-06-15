f0c3e0888c2749fdc45d2823aba4e03c86aba622
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-06-15 19:18:42 +0300

    Added problem for Library in IteratorsAndComparators exercise.

---
+++ JavaAdvanced/src/IteratorsAndComparators/Lab/Library_02/Main.java
@@ -0,0 +1,26 @@
+package IteratorsAndComparators.Lab.Library_02;
+
+import IteratorsAndComparators.Lab.Book_01.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        Library library = new Library();
+        library.add(bookOne);
+        library.add(bookTwo);
+        library.add(bookThree);
+
+        for (Book book : library) {
+            System.out.println(book.getTitle());
+        }
+
+    }
+}
