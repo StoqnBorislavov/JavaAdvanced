46b203607d329ee2d45e0a3d52bff5516e4f6387
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-07-05 15:10:05 +0300

    Directories changing;

--- JavaAdvanced/src/Generics/Lab/GenericArrayCreator_02/Main.java
+++
@@ -1,9 +0,0 @@
-package Generics.Lab.GenericArrayCreator_02;
-
-public class Main {
-    public static void main(String[] args) {
-
-        String[] javas = ArrayCreator.<String>create(String.class, 13, "Java");
-        
-    }
-}
--- JavaAdvanced/src/Generics/Lab/GenericScale_03/Main.java
+++
@@ -1,9 +0,0 @@
-package Generics.Lab.GenericScale_03;
-
-public class Main {
-    public static void main(String[] args) {
-        Scale<String> scale = new Scale<>("a", "z");
-
-        System.out.println(scale.getHeavier());
-    }
-}
