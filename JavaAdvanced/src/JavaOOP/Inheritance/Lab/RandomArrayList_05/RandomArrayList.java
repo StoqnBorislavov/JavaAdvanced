40fdeb4487a6e339e9cf599d4fc4984a08376eb9
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-07-06 21:18:52 +0300

    Added problem with solutions in Inheritance Lab.

---
+++ JavaAdvanced/src/JavaOOP/Inheritance/Lab/RandomArrayList_05/RandomArrayList.java
@@ -0,0 +1,13 @@
+package JavaOOP.Inheritance.Lab.RandomArrayList_05;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList<Object> {
+
+
+    public Object getRandomElement(){
+        int index = new Random().nextInt(super.size());
+        return super.get(index);
+    }
+}
