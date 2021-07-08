1943639e43dfc7898757772c2ea688ec23450744
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-07-08 20:47:35 +0300

    Added problem for Restaurant with solution in Inheritance Exercise.

---
+++ JavaAdvanced/src/JavaOOP/Inheritance/Exercise/Restaurant/Coffee.java
@@ -0,0 +1,22 @@
+package JavaOOP.Inheritance.Exercise.Restaurant;
+
+import java.math.BigDecimal;
+
+public class Coffee extends HotBeverage{
+//•	double COFFEE_MILLILITERS = 50
+//•	BigDecimal COFFEE_PRICE = 3.50
+//•	caffeine – double
+//•	Getter for caffeine
+    private static final double COFFEE_MILLILITERS = 50;
+    private static final BigDecimal COFFEE_PRICE = new BigDecimal(3.50);
+    private double caffeine;
+
+    public Coffee(String name, double caffeine){
+        super(name, COFFEE_PRICE, COFFEE_MILLILITERS);
+        this.caffeine = caffeine;
+    }
+
+    public double getCaffeine() {
+        return caffeine;
+    }
+}
