576d68a6488264c6f77b0dbb6ac925ae46b22bd0
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-07-13 20:49:47 +0300

    Added problems with solutions in Polymorphism Lab.

---
+++ JavaAdvanced/src/JavaOOP/Polymorphism/Lab/MathOperation_01/MathOperation.java
@@ -0,0 +1,13 @@
+package JavaOOP.Polymorphism.Lab.MathOperation_01;
+
+public class MathOperation {
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int add(int num1, int num2, int num3){
+        return add(add(num1, num2), num3);
+    }
+    public static int add(int num1, int num2, int num3, int num4){
+        return add(add(num1, num2), add(num3, num4));
+    }
+}
